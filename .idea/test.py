bc43fc1b97d1d80f704dd2409980c560c0d89eaa
Jim Waldo <jiniwaldo@gmail.com>
2019-03-04 16:58:48 -0500

    First version that separately tracks students moving from Cambridge to Allston and those moving from Allston to Cambridge. Added a close-to-full README file. Added lots of documentation to the .py files. Extracted routines in a number of the python files so that the whole cycle could be run from a single program.

---
+++ .idea/test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/evn python3
+# -*- coding: utf-8 -*-
+"""
+Created on 
+
+@author 
+
+"""
---
+++ .idea/test1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/evn python3
+# -*- coding: utf-8 -*-
+"""
+Created on 2019-01-05
+
+@author waldo
+
+"""
